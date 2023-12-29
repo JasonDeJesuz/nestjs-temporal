0dc9f47c402278d17b186bd2e285b3d4663ab9d2
Jason De Jesuz <dejesuzjason@gmail.com>
2023-12-29 11:48:11 +0200

    expose worker in temporal explorer

--- lib/temporal.explorer.ts
+++ lib/temporal.explorer.ts
@@ -28,7 +28,10 @@ export class TemporalExplorer
   implements OnModuleInit, OnModuleDestroy, OnApplicationBootstrap {
   @Inject(TEMPORAL_MODULE_OPTIONS_TOKEN) private options: TemporalModuleOptions;
   private readonly logger = new Logger(TemporalExplorer.name);
-  private worker: Worker;
+  public get worker() {
+    return this._worker;
+  }
+  private _worker: Worker;
   private timerId: ReturnType<typeof setInterval>;
 
   constructor(
@@ -90,7 +93,7 @@ export class TemporalExplorer
       }
 
       this.logger.verbose('Creating a new Worker');
-      this.worker = await Worker.create(
+      this._worker = await Worker.create(
         Object.assign(workerOptions, workerConfig),
       );
     }
